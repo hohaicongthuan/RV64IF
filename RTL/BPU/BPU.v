a9cf13fece0f46b47d3683332cfaa0e85f882a3f
hohaicongthuan <hohaicongthuan@gmail.com>
2022-04-03 15:31:43 +0700

    Update BPU

--- RTL/BPU/BPU.v
+++ RTL/BPU/BPU.v
@@ -1,6 +1,35 @@
 // Branch Prediction Unit
 
-module BPU(in_PC, in_hit, out_predict);
-    input   in_PC, in_hit;
-    output  out_predict;
+module BPU(in_PC, in_hit, in_Clk, in_Rst_N, out_prediction);
+    input   in_Clk, in_Rst_N;
+    input   [1:0] in_hit;   // LSB defines if predictor 0 hit or not, MSB defines if predictor 1 hit or not
+    input   [8:0] in_PC;
+    output  out_prediction;
+
+    wire    predictor_selector_Out, bimodal_prediction, gshare_prediction;
+
+    assign out_prediction = (predictor_selector_Out) ? gshare_prediction : bimodal_prediction;
+
+    predictor_sel predictor_sel_Inst0(
+        .in_hit(in_hit),
+        .in_Clk(in_Clk),
+        .in_Rst_N(in_Rst_N),
+        .out_selection(predictor_selector_Out)
+    );
+
+    bimodal bimodal_Inst0(
+        .in_PC(in_PC),
+        .in_hit(in_hit[0]),
+        .in_Clk(in_Clk),
+        .in_Rst_N(in_Clk),
+        .out_prediction(bimodal_prediction)
+    );
+
+    gshare gshare_Inst0(
+        .in_PC(in_PC),
+        .in_hit(in_hit[1]),
+        .in_Clk(in_Clk),
+        .in_Rst_N(in_Rst_N),
+        .out_prediction(gshare_prediction)
+    );
 endmodule
---
+++ RTL/BPU/predictor_sel.v
@@ -0,0 +1,21 @@
+// Predictor Selector selects which branch predictor to use
+
+module predictor_sel(in_hit, in_Clk, in_Rst_N, out_selection);
+    input   in_Clk, in_Rst_N;
+    input   [1:0] in_hit;
+
+    output  out_selection;
+
+    // 00: both predictors not hit              -> 0 (move towards predictor 0)
+    // 01: predictor 1 not hit, predictor 0 hit -> 0 (move towards predictor 0)
+    // 10: predictor 1 hit, predictor 0 not hit -> 1 (move towards predictor 1)
+    // 11: both predictors hit                  -> 1 (move towards predictor 1)
+
+    sat_counter sat_counter_Inst0(
+        .in_data(in_hit[1]),
+        .out_prediction(out_selection),
+        .in_Clk(in_Clk),
+        .in_Rst_N(in_Rst_N),
+        .in_En(1'b1)
+    );
+endmodule
