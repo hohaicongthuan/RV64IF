aea1113d8fa5e15a6e33d2783d00cdc23140fbe4
hohaicongthuan <hohaicongthuan@gmail.com>
2022-04-12 16:53:13 +0700

    Update FP_Div test instructions

--- Testbench/Test_Instructions/FP_Div_Instruction_Test/Test_Instructions.s
+++ Testbench/Test_Instructions/FP_Div_Instruction_Test/Test_Instructions.s
@@ -1,8 +1,7 @@
 main:
-    li t1, 0x42F6E979               # t1 = 123.456
-    li t2, 0x414570A4               # t2 = 12.34
-    li t3, 0x3C23D70A               # t3 = 0.01
-    li t4, 0x41201297               # t4 = 10.004539
+    nop
+    li t1, 0x414570A4               # t1 = 12.34
+    li t2, 0x3F9DF3B6               # t2 = 1.234
     nop
     nop
     nop
@@ -10,29 +9,14 @@ main:
     nop
     fmv.w.x ft1, t1    
     fmv.w.x ft2, t2
-    fmv.w.x ft4, t4
-    fmv.w.x ft6, t3
     nop
     nop
     nop
     nop
     nop
-    fdiv.s ft3, ft1, ft2            # ft3 = ft1 / ft2 = 123.456 / 12.34 = 10.004539
+    fdiv.s ft3, ft1, ft2            # ft3 = ft1 / ft2 = 12.34 / 1.234 = 10.0 (0x41200000)
     nop
     nop
     nop
     nop
     nop
-    fsub.s ft5, ft3, ft4
-    nop
-    nop
-    nop
-    nop
-    nop
-    fle.s t0, ft5, ft6
-    nop
-    nop
-    nop
-    nop
-    nop
-    sd t0, 0(sp)
